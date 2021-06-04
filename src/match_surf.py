7f0ae09cd3aa5b420e2c52e318c0b4311d7e09de
icardemil64 <icardemil64@gmail.com>
2021-06-04 18:13:40 -0400

    Fracasando en SURF.

--- src/match_surf.py
+++ src/match_surf.py
@@ -1,2 +1,17 @@
 import cv2
-print(cv2.__version__)
+# Descargar una versión que tenga el método de SURF 😞.
+"""
+    img1 = cv2.imread("../img/picadef1.png")
+
+    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+
+    surf = cv2.xfeatures2d.SURF_create(8000)
+    kp1, dp1 = surf.detectAndCompute(gray, None)
+
+    img1 = cv2.drawKeypoints(image=img1, outImage=img1, keypoints=kp1, flags=4, color=(51,163,236))
+    print(surf.descriptorSurf())
+
+    cv2.imshow('Keypoints', img1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+"""
