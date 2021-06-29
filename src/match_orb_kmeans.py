35c14a5b46173b585377e1f677c30b17eec127d4
icardemil64 <icardemil64@gmail.com>
2021-06-28 23:38:10 -0400

    Corrección en el nombre de los archivos

--- src/match_orb_kmeans.py
+++ src/match_orb_kmeans.py
@@ -1,55 +1,68 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+# Recibe dos puntos (P,Q) y los centroides de cada uno para calcular la distancia
+# y realizar la comparación para filtrar la lista de puntos con los que se realizará
+# el matching.
+def validarPuntos(p,q,c_1,c_2,umbral):
+    p_x,p_y,p_i = p
+    q_x,q_y,q_i = q
+    c_x_p,c_y_p,c_i_p = c_1[0]
+    c_x_q,c_y_q,c_i_q = c_2[0]
+    if abs(math.dist([p_x,p_y],[c_x_p,c_y_p]) - math.dist([q_x,q_y],[c_x_q,c_y_q])) < umbral:
+        return True
+    else:
+        return False
 
 img1 = cv2.imread("img/picadef1.png")
-img2 = cv2.imread("img/picadef4.png")
+img2 = cv2.imread("img/picadef3.png")
 
 #ORB Detector
 orb = cv2.ORB_create()
 kp1,des1 = orb.detectAndCompute(img1,None)
 kp2,des2 = orb.detectAndCompute(img2,None)
-
+#print(kp1.__dir__())
 #Brute Force Matching
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 matches = bf.match(des1,des2)
 
 #Obtener las coordenadas del brute force matching
-list_kp = []
-
+list_kp1 = []
+list_kp2 = []
 for m in matches:
-    img1_idx = m.queryIdx
-    img2_idx = m.trainIdx
-
+    img1_index = m.queryIdx
+    img2_index = m.trainIdx
     #Coordenadas
-    (x1,y1) = kp1[img1_idx].pt
-    #(x2,y2) = kp2[img2_idx].pt
-    list_kp.append((x1,y1))
-    #list_kp.append((x2,y2))
+    (x1,y1) = kp1[img1_index].pt
+    (x2,y2) = kp2[img2_index].pt
+    list_kp1.append((x1,y1,img1_index))
+    list_kp2.append((x2,y2,img2_index))
+
 
 #K-Means
-list_kp = np.float32(np.vstack(list_kp))
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
 
-ret, label, center = cv2.kmeans(list_kp, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
-#aTX,aTY = center[1]
-#print((aTX-25,aTY-25),(aTX-25,aTY+25),(aTX+25,aTY-25),(aTX+25,aTY+25))
+#Primera imagen
+list_kp1 = np.float32(np.vstack(list_kp1))
+ret_1, label_1, center_1 = cv2.kmeans(list_kp1, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-A = list_kp[label.ravel() == 0]
-#B = list_kp[label.ravel() == 1]
+#Seguna imagen
+list_kp2 = np.float32(np.vstack(list_kp2))
+ret_2, label_2, center_2 = cv2.kmeans(list_kp2, 1, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
-fig = plt.figure(figsize=(12, 6))
-plt.suptitle("K-means clustering algorithm", fontsize=14, fontweight='bold')
-fig.patch.set_facecolor('silver')
+#Calcular las distancia
+list_validos = []
+for i in range(len(list_kp1)):
+    if validarPuntos(list_kp1[i],list_kp2[i],center_1,center_2,30):
+        list_validos.append(matches[i])
+    
+print(len(matches))
+print(len(list_validos))
 
-ax = plt.subplot(1, 2, 1)
-plt.scatter(list_kp[:, 0], list_kp[:, 1], c='c')
-plt.title("data")
+result = cv2.drawMatches(img1, kp1, img2, kp2, list_validos, None, flags=2)
 
-ax = plt.subplot(1, 2, 2)
-plt.scatter(A[:, 0], A[:, 1], c='b')
-#plt.scatter(B[:, 0], B[:, 1], c='g')
-plt.scatter(center[:, 0], center[:, 1], s=100, c='m', marker='s')
-plt.title("clustered data and centroids (K = 1)")
-
-plt.show()
+cv2.imshow("Detector ORB + KMeans",result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
